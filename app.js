52dfadb6ff216e77f0baad4fe81e0a3b007b7649
SebasLam3 <90872517+SebasLam3@users.noreply.github.com>
2022-03-21 12:45:14 -0500

    puzzle

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 // Estado inicial puzzle
-let iniciales = [];
-let finales = [];
+var EstadoInicial = [];
+var EstadoFinal = [];
 
-function setEstados(){
-    var in1 = document.getElementById('i1').value;
-    var in2 = document.getElementById('i2').value;
-    var in3 = document.getElementById('i3').value;
-    var in4 = document.getElementById('i4').value;
-    var in5 = document.getElementById('i5').value;
-    var in6 = document.getElementById('i6').value;
-    var in7 = document.getElementById('i7').value;
-    var in8 = document.getElementById('i8').value;
-    var in9 = document.getElementById('i9').value;
-    iniciales.push(in1);
+/*function setEstados(){
+*/
+    let in1 = document.getElementById("i1");
+    let in2 = document.getElementById("i2");
+    let in3 = document.getElementById("i3");
+    let in4 = document.getElementById("i4");
+    let in5 = document.getElementById("i5");
+    let in6 = document.getElementById("i6");
+    let in7 = document.getElementById("i7");
+    let in8 = document.getElementById("i8");
+    let in9 = document.getElementById("i9");
+    /*iniciales.push(in1);
     iniciales.push(in2);
     iniciales.push(in3);
     iniciales.push(in4);
@@ -21,18 +22,18 @@ function setEstados(){
     iniciales.push(in7);
     iniciales.push(in8);
     iniciales.push(in9);
-   console.log(iniciales);
-
-   var fn1 = document.getElementById('f1').value;
-   var fn2 = document.getElementById('f2').value;
-   var fn3 = document.getElementById('f3').value;
-   var fn4 = document.getElementById('f4').value;
-   var fn5 = document.getElementById('f5').value;
-   var fn6 = document.getElementById('f6').value;
-   var fn7 = document.getElementById('f7').value;
-   var fn8 = document.getElementById('f8').value;
-   var fn9 = document.getElementById('f9').value;
-   finales.push(fn1);
+    console.log(iniciales);
+*/
+   let fn1 = document.getElementById("f1");
+   let fn2 = document.getElementById("f2");
+   let fn3 = document.getElementById("f3");
+   let fn4 = document.getElementById("f4");
+   let fn5 = document.getElementById("f5");
+   let fn6 = document.getElementById("f6");
+   let fn7 = document.getElementById("f7");
+   let fn8 = document.getElementById("f8");
+   let fn9 = document.getElementById("f9");
+   /*finales.push(fn1);
    finales.push(fn2);
    finales.push(fn3);
    finales.push(fn4);
@@ -41,21 +42,52 @@ function setEstados(){
    finales.push(fn7);
    finales.push(fn8);
    finales.push(fn9);
-   console.log(finales);
+   console.log(finales);*/
+
+    /*return [iniciales, finales];
+}*/
+
+let guardarInicial = function(){
+
+     EstadoInicial = [
+        in1.value, in2.value, in3.value,
+        in4.value, in5.value, in6.value, // 0 Para nulo
+        in7.value, in8.value, in9.value 
+    ];
+    var EI = [];
+    for (var i = 0; i < EstadoInicial.length; i++){
+        EI.push(parseInt(EstadoInicial[i]));
+    }
+    console.log(EI);
+    return EI;
 }
 
-let EstadoInicial = [
-    iniciales[0], iniciales[1], iniciales[2],
-    iniciales[3], iniciales[4], iniciales[5], // 0 Para nulo
-    iniciales[6], iniciales[7], iniciales[8]
+function guardarFinal(){
+    EstadoFinal = [
+        fn1.value, fn2.value, fn3.value,
+        fn4.value, fn5.value, fn6.value,
+        fn7.value, fn8.value, fn9.value
+    ];
+    var EF = [];
+    for (var i = 0; i < EstadoFinal.length; i++){
+        EF.push(parseInt(EstadoFinal[i]));
+    }
+    console.log(EF);
+    return EF;
+}
+/*let Ini = [
+    1, 0, 4,
+    2, 6, 3, // 0 para nulo
+    8, 5, 7
 ];
+console.log(Ini);
 
-let EstadoFinal = [
-    finales[0], finales[1], finales[2],
-    finales[3], finales[4], finales[5],
-    finales[6], finales[7], finales[8]
+let Fin = [
+    1, 6, 4,
+    2, 3, 7,
+    8, 5, 0
 ];
-
+console.log(Fin);*/
 //Nodos
 let allNodes = {};
 
@@ -69,8 +101,10 @@ function swap(value, swapfrom, swapto) {
 
 
 function checkEstadoFinal(value) {
+    var x = guardarFinal();
+    var Ef = x;
     // valor es una matriz
-    if (JSON.stringify(EstadoFinal).indexOf(JSON.stringify(value)) >= 0) {
+    if (JSON.stringify(Ef).indexOf(JSON.stringify(value)) >= 0) {
         return true;
     } else {
         return false;
@@ -100,10 +134,17 @@ function MovimientosPos(value) {
 let NodosVisitados = [];
 let Cola = [];
 
-// Agregando inicial a la cola 
-Cola.push(EstadoInicial);
+//console.log(EstadoInicial);
+//console.log(EstadoFinal);
 
-let main = function () {
+function main () {
+    //guardar(); 
+    
+    var y = guardarInicial();
+    var Ei = y;
+    // Agregando inicial a la cola 
+    Cola.push(Ei);
+    console.log(Cola);
     let index = Cola[0].indexOf(0);
     let moves = MovimientosPos(index);
 
@@ -146,12 +187,10 @@ let main = function () {
         });
 
         output.innerHTML = t;
-
+        
         console.log(NodosVisitados.length);
-
         return;
     } else {
         main();
     }
 }
-//window.addEventListener('load', main);

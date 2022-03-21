ba2213ab1e59de246f7e83c9668605097abf47c4
SebasLam3 <90872517+SebasLam3@users.noreply.github.com>
2022-03-21 00:28:05 -0500

    Puzzle

--- app.js
+++ app.js
@@ -1,20 +1,65 @@
-// INITIAL STATE OF THE 8 PUZZLE
-let initialState = [
-    1, 6, 4,
-    2, 0, 3, // 0 FOR NULL
-    8, 5, 7
+// Estado inicial puzzle
+let iniciales = [];
+let finales = [];
+
+function setEstados(){
+    var in1 = document.getElementById('i1').value;
+    var in2 = document.getElementById('i2').value;
+    var in3 = document.getElementById('i3').value;
+    var in4 = document.getElementById('i4').value;
+    var in5 = document.getElementById('i5').value;
+    var in6 = document.getElementById('i6').value;
+    var in7 = document.getElementById('i7').value;
+    var in8 = document.getElementById('i8').value;
+    var in9 = document.getElementById('i9').value;
+    iniciales.push(in1);
+    iniciales.push(in2);
+    iniciales.push(in3);
+    iniciales.push(in4);
+    iniciales.push(in5);
+    iniciales.push(in6);
+    iniciales.push(in7);
+    iniciales.push(in8);
+    iniciales.push(in9);
+   console.log(iniciales);
+
+   var fn1 = document.getElementById('f1').value;
+   var fn2 = document.getElementById('f2').value;
+   var fn3 = document.getElementById('f3').value;
+   var fn4 = document.getElementById('f4').value;
+   var fn5 = document.getElementById('f5').value;
+   var fn6 = document.getElementById('f6').value;
+   var fn7 = document.getElementById('f7').value;
+   var fn8 = document.getElementById('f8').value;
+   var fn9 = document.getElementById('f9').value;
+   finales.push(fn1);
+   finales.push(fn2);
+   finales.push(fn3);
+   finales.push(fn4);
+   finales.push(fn5);
+   finales.push(fn6);
+   finales.push(fn7);
+   finales.push(fn8);
+   finales.push(fn9);
+   console.log(finales);
+}
+
+let EstadoInicial = [
+    iniciales[0], iniciales[1], iniciales[2],
+    iniciales[3], iniciales[4], iniciales[5], // 0 Para nulo
+    iniciales[6], iniciales[7], iniciales[8]
 ];
 
-let goalState = [
-    1, 6, 4,
-    2, 3, 7,
-    8, 5, 0
+let EstadoFinal = [
+    finales[0], finales[1], finales[2],
+    finales[3], finales[4], finales[5],
+    finales[6], finales[7], finales[8]
 ];
 
-// ALL NODES
+//Nodos
 let allNodes = {};
 
-// MOVE FUNCTIONS
+// Funciones de Movimiento
 function swap(value, swapfrom, swapto) {
     let temp = value[swapto];
     value[swapto] = 0;
@@ -23,17 +68,17 @@ function swap(value, swapfrom, swapto) {
 }
 
 
-function checkGoalState(value) {
-    // value is array
-    if (JSON.stringify(goalState).indexOf(JSON.stringify(value)) >= 0) {
+function checkEstadoFinal(value) {
+    // valor es una matriz
+    if (JSON.stringify(EstadoFinal).indexOf(JSON.stringify(value)) >= 0) {
         return true;
     } else {
         return false;
     }
 }
 
-// GET POSSIBLE MOVES
-function getPossibleMoves(value) {
+// Obteniendo posibles movimientos
+function MovimientosPos(value) {
     let mid = {
         0: [{ swap: 1 }, { swap: 3 }],
         1: [{ swap: 2 }, { swap: 4 }, { swap: 0 }],
@@ -44,47 +89,53 @@ function getPossibleMoves(value) {
         6: [{ swap: 3 }, { swap: 7 }],
         7: [{ swap: 4 }, { swap: 8 }, { swap: 6 }],
         8: [{ swap: 5 }, { swap: 7 }]
+        
     }
-
+    
+    console.log(mid);
     return mid[value];
+   
 };
 
-let visitedNodes = [];
-let Queue = [];
+let NodosVisitados = [];
+let Cola = [];
 
-// ADD INITAL TO QUEUE
-Queue.push(initialState);
+// Agregando inicial a la cola 
+Cola.push(EstadoInicial);
 
 let main = function () {
-    let index = Queue[0].indexOf(0);
-    let moves = getPossibleMoves(index);
+    let index = Cola[0].indexOf(0);
+    let moves = MovimientosPos(index);
 
-    // GET CHILDS
+    // Obteniendo nodos hijos
     let childNodes = moves.map(item => {
-        let temp = [...Queue[0]];
+        let temp = [...Cola[0]];
         return swap(temp, index, item.swap);
     });
 
     childNodes.forEach(item => {
-        if (JSON.stringify(Queue).indexOf(JSON.stringify(item)) == -1) {
-            // IF NOT FOUND ON QUEUE THEN INSERT IN QUEUE AND NOT IN VISITED
-            if (JSON.stringify(visitedNodes).indexOf(JSON.stringify(item)) == -1) {
-                Queue.push(item);
+        if (JSON.stringify(Cola).indexOf(JSON.stringify(item)) == -1) {
+            // Si no esta en la cola, se inserta en cola y en los no visitados 
+            if (JSON.stringify(NodosVisitados).indexOf(JSON.stringify(item)) == -1) {
+                Cola.push(item);
+                console.log(Cola);
             }
         }
     });
 
-    let visited = Queue.splice(0, 1)[0];
-    visitedNodes.push(visited);
+    let visitados = Cola.splice(0, 1)[0];
+    
+    console.log(visitados); 
+    NodosVisitados.push(visitados);
 
-    if (checkGoalState(visited)) {
-        console.log("FOUND");
-        console.log(visitedNodes);
+    if (checkEstadoFinal(visitados)) {
+        console.log("ENCONTRADO");
+        console.log(NodosVisitados);
 
         let output = document.getElementById("output");
 
         let t = '';
-        visitedNodes.forEach((item) => {
+        NodosVisitados.forEach((item) => {
 
             t += '<div class="node">';
             item.forEach(value => {
@@ -96,12 +147,11 @@ let main = function () {
 
         output.innerHTML = t;
 
-        console.log(visitedNodes.length);
+        console.log(NodosVisitados.length);
 
         return;
     } else {
         main();
     }
 }
-
-window.addEventListener('load', main);
+//window.addEventListener('load', main);
